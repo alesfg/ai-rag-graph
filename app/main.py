9fbf47b000c53823deefafa15d12b64994b8c70b
alesfg <alex.f.yo@hotmail.com>
2026-04-07 19:48:50 +0200

    feat: add text upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Simulación de "base de datos"
+db = None
+
+
+class TextInput(BaseModel):
+    text: str
 
 
 @app.get("/")
 def root():
     return {"message": "API running"}
+
+
+@app.post("/upload")
+def upload_text(input: TextInput):
+    global db
+    db = input.text  # de momento solo guardamos el texto
+    return {
+        "status": "stored",
+        "length": len(input.text)
+    }
